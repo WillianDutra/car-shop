7e86796c8eb8a31fb79795f8ceb4a7ae276f130a
Willian Dutra <contatowilliandutra@gmail.com>
2023-04-25 23:32:56 -0300

    test: Implementando testes na camada Service de Motorcycle

---
+++ tests/unit/Services/MotorcycleService.mock.ts
@@ -0,0 +1,38 @@
+export const inputValue = {
+  model: 'Gol bolinha',
+  year: 2000,
+  color: 'Black',
+  status: true,
+  buyValue: 15.000,
+  category: 'Street',
+  engineCapacity: 5,
+};
+
+export const motorcycleId = {
+  id: '64487f431852311ef2e534b3',
+};
+
+export const motorcycleWithId = { ...motorcycleId, ...inputValue };
+
+export const motorcycleList = [
+  {
+    id: '64487f431852311ef2e534b3',
+    model: 'Honda CG 125',
+    year: 2010,
+    color: 'Black',
+    status: true,
+    buyValue: 15.000,
+    category: 'Street',
+    engineCapacity: 300,
+  },
+  {
+    id: '64457z441862712ff2e534b2',
+    model: 'Honda Cb 600f Hornet',
+    year: 2005,
+    color: 'White',
+    status: true,
+    buyValue: 25.000,
+    category: 'Street',
+    engineCapacity: 600,
+  },
+];
---
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -0,0 +1,86 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+
+import MotorcycleService from '../../../src/Services/MotorcycleService';
+import {
+  inputValue, motorcycleId, motorcycleList, motorcycleWithId,
+} from './MotorcycleService.mock';
+
+describe('Testa a camada Service de Motorcycle', function () {
+  describe('Casos de SUCESSO', function () {
+    it('Consegue cadastrar uma motocicleta', async function () {
+      // Arrange
+      sinon.stub(Model, 'create').resolves(motorcycleWithId);
+      // Act
+      const service = new MotorcycleService();
+      const result = await service.create(inputValue);
+      // Assert
+      expect(result).to.be.deep.equal(motorcycleWithId);
+    });
+
+    it('Consegue fazer uma consulta por todas as motocicletas', async function () {
+      // Arrange
+      sinon.stub(Model, 'find').resolves(motorcycleList);
+      // Act
+      const service = new MotorcycleService();
+      const result = await service.getAll();
+      // Assert
+      expect(result).to.be.deep.equal(motorcycleList);
+    });
+
+    it('Consegue fazer uma consulta pelo ID da motocicleta', async function () {
+      // Arrange
+      sinon.stub(Model, 'findOne').resolves(motorcycleWithId);
+      // Act
+      const service = new MotorcycleService();
+      const result = await service.getById(motorcycleId.id);
+      // Assert
+      expect(result).to.be.deep.equal(motorcycleWithId);
+    });
+
+    it('Consegue fazer um update de uma motocicleta', async function () {
+      // Arrange
+      sinon.stub(Model, 'findByIdAndUpdate').resolves(motorcycleWithId);
+      // Act
+      const service = new MotorcycleService();
+      const result = await service.update(motorcycleId.id, inputValue);
+      // Assert
+      expect(result).to.be.deep.equal(motorcycleWithId);
+    });
+  });
+
+  describe('Casos de ERRO', function () {
+    it('Retornar erro quando ID for invalido', async function () {
+      // Arrange
+      sinon.stub(Model, 'findOne').resolves({});
+      
+      try {
+        // Act
+        const service = new MotorcycleService();
+        await service.getById('INVALID_ID');
+      } catch ({ message }) {
+        // Assert
+        expect(message).to.be.deep.equal('Invalid mongo id');
+      }
+    });
+
+    it('Retorna erro quando nenhum motocicleta foi encontrada', async function () {
+      // Arrange
+      sinon.stub(Model, 'findOne').resolves({});
+      
+      try {
+        // Act
+        const service = new MotorcycleService();
+        await service.getById(motorcycleId.id);
+      } catch ({ message }) {
+        // Assert
+        expect(message).to.be.deep.equal('Motorcycle not found');
+      }
+    });
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
